74592c1797c1a987872cb0a967099b3a9e4c4b01
omergal99 <omergal99gm@gmail.com>
2019-07-12 20:30:24 +0300

    omer- update msg

---
+++ public/build/precache-manifest.d3f07c7ecf5125e1e0284b780fe42d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1422ab3451e532d0888da78632dc300f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f5640c7ebce9d136e0",
+    "url": "/static/css/main.76103252.chunk.css"
+  },
+  {
+    "revision": "8842808b308c86d7f8f6",
+    "url": "/static/js/2.98270cee.chunk.js"
+  },
+  {
+    "revision": "80f5640c7ebce9d136e0",
+    "url": "/static/js/main.a0e7fb20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/static/media/Quando-Regular.849e6795.ttf"
+  }
+]);
